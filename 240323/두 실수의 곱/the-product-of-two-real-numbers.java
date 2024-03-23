1ef2619392f7cfb988b0d61e1997d7349a91efa3
Choi mieun <45choi22@gmail.com>
2024-03-23 12:48:04 +0900

    Title: 두 실수의 곱, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240323/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        float a = 5.26f;
+        float b = 8.27f;
+
+        float c = a*b;
+
+        System.out.printf("%.3f",c);
+    }
+}
