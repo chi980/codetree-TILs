6ed5d7817e48d0886ea677177daa1e43117dcfbb
Choi mieun <45choi22@gmail.com>
2024-03-27 19:49:04 +0900

    Title: 구구단으로 이루어진 직각 삼각형, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240327/구구단으로 이루어진 직각 삼각형/a-right-triangle-made-up-of-multiplication-tables.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.printf("%d * %d = %d", 5-i+1, j, (5-i+1)*j);
+                if(i!=j) System.out.print(" / ");
+                else System.out.print("\n");
+            }
+            // System.out.println();
+        }
+    }
+}
