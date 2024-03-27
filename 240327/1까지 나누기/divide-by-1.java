2b59a99236a3b45cc22a39ce5104ebcab8acb013
Choi mieun <45choi22@gmail.com>
2024-03-27 18:01:00 +0900

    Title: 1까지 나누기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240327/1까지 나누기/divide-by-1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        for(int i=1;;i++){
+            if(n <= 1){
+                System.out.println(i-1);
+                break;
+            }
+
+            n /= i;
+
+        }
+    }
+}
