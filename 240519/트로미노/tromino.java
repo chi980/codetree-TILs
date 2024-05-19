1df9386a4c8d271f265fe742a6158f7fe012de22
Choi mieun <45choi22@gmail.com>
2024-05-19 16:17:46 +0900

    Title: 트로미노, Time: 172ms, Memory: 12MB, Status: Passed - CodeTree

--- 240519/트로미노/tromino.java
+++ 240519/트로미노/tromino.java
@@ -22,50 +22,48 @@ public class Main {
         }
 
         int maxCost = 0;
+        boolean[][] visited = new boolean[R][C];
         for(int r=0;r<R;r++){
             for(int c=0;c<C;c++){
-                int cost = BFS(R, C, map, r, c);
+                visited[r][c] = true;
+                int cost = DFS(R, C, map, r, c, 0, visited);
                 maxCost = Math.max(maxCost, cost);
+                visited[r][c] = false;
             }
         }
         System.out.println(maxCost);
     }
 
-    static int BFS(int R, int C, int[][] map, int startr, int startc){
-        int max = 0;
-
-        int[][] visited = new int[R][C];
-        Queue<int[]> q = new ArrayDeque<>();
-
-        visited[startr][startc] = 1;
-        q.offer(new int[]{startr, startc, map[startr][startc]});
-
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int curr = cur[0];
-            int curc = cur[1];
-            int cursum = cur[2];
-
-            for(int d=0;d<4;d++){
-                int newr = curr+dr[d];
-                int newc = curc+dc[d];
-
-                if(!check(R, C, newr, newc)) continue;
-                if(visited[newr][newc]!=0) continue;
-
-                visited[newr][newc] = visited[curr][curc]+1;
-                if(visited[newr][newc] == 3){
-                    max = Math.max(max, cursum + map[newr][newc]);
-                }else{
-                    q.offer(new int[]{newr, newc, cursum + map[newr][newc]});
-                }
-            }
+    static int DFS(int R, int C, int[][] map, int curr, int curc, int depth, boolean[][] visited){
+        if(depth == 2){
+            return map[curr][curc];
         }
-        return max;
+
+        int maxCost = 0;
+        for(int d=0;d<4;d++){
+            int newr = curr+dr[d];
+            int newc = curc+dc[d];
+
+            if(!check(R, C, newr, newc)) continue;
+            if(visited[newr][newc]) continue;
+
+            visited[newr][newc] = true;
+            int curCost = DFS(R, C, map, newr, newc, depth+1, visited);
+            maxCost = Math.max(maxCost, curCost);
+            visited[newr][newc] = false;
+        }
+
+        return maxCost+map[curr][curc];
     }
+
 
     static boolean check(int R, int C, int r, int c){
         if(r< 0 || c < 0 || r>=R || c >=C) return false;
         return true;
+    }
+
+    static void print(int[][] arr){
+        for(int i=0;i<arr.length;i++)System.out.println(Arrays.toString(arr[i]));
+        System.out.println();
     }
 }
