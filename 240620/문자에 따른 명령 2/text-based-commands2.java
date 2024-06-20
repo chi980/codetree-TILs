40fefab8ac911a0833f187d76a475e5925851d28
Choi mieun <45choi22@gmail.com>
2024-06-20 14:52:41 +0900

    Title: 문자에 따른 명령 2, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 240620/문자에 따른 명령 2/text-based-commands2.java
+++ 240620/문자에 따른 명령 2/text-based-commands2.java
@@ -3,28 +3,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
-	public static void main(String[] args) throws IOException{
+	static int[] dy = { 1, 0, -1, 0 };
+	static int[] dx = { 0, 1, 0, -1 };
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char[] dNums = br.readLine().toCharArray();
-		
-		int r = 0;
-		int c = 0;
+
+		int x = 0;
+		int y = 0;
 		int d = 0;
 		for (int i = 0; i < dNums.length; i++) {
-			if(dNums[i] == 'L') {
+			if (dNums[i] == 'L') {
 				d += 3;
 				d %= 4;
-			}else if(dNums[i] == 'R') {
+			} else if (dNums[i] == 'R') {
 				d += 1;
 				d %= 4;
-			}else if(dNums[i] == 'F') {
-				r = r+dr[d];
-				c = c + dc[d];
+			} else if (dNums[i] == 'F') {
+				x = x + dx[d];
+				y = y + dy[d];
 			}
 		}
-		System.out.println(c+" "+r);
-		
+		System.out.println(x + " " + y);
+
 	}
 }
