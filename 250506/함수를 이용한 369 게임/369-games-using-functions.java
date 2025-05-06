3650ac93aa7e7b8d05ef8305b88c183bc4f7177e
Choi mieun <45choi22@gmail.com>
2025-05-06 20:14:00 +0900

    Title: 함수를 이용한 369 게임, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 250506/함수를 이용한 369 게임/369-games-using-functions.java
+++ 250506/함수를 이용한 369 게임/369-games-using-functions.java
@@ -8,13 +8,16 @@ public class Main {
 
         int cnt = 0;
         for(int i=A;i<=B;i++){
-            if(isCorrect(i)) cnt++;
+            if(is369Number(i)) cnt++;
         }
 
         System.out.println(cnt);
     }
 
-    public static boolean isCorrect(int n){
+    /**
+    *. toString()은 내부적으로 문자열 객체 생성 + 문자 배열 생성까지 동반 → 오버헤드 있음
+    **/
+    public static boolean is369NumberByArray(int n){
         if(n % 3 == 0) return true;
 
         char[] charArray = Integer.toString(n).toCharArray();
@@ -24,5 +27,14 @@ public class Main {
         }
         return false;
     }
+    public static boolean is369Number(int n){
+        if(n % 3 == 0) return true;
 
+        while(n > 0){
+            if(n % 10 == 3 || n % 10 == 6 || n % 10 == 9)
+                return true;
+            n /= 10;
+        }
+        return false;
+    }
 }
